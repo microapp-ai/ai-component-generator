74096e9e06e3aff97f99dbf753de874c62c2c1fc
Carlos Knopel <carlos.knopel@gmail.com>
2023-03-10 13:56:44 -0300

    fix: fix cors

--- cors.js
+++ cors.js
@@ -1,7 +1,8 @@
 import cors from 'vercel-cors';
 
 export default cors({
-  origin: '*',
+  origin:
+    process.env.NODE_ENV === 'development' ? '*' : 'https://www.microapp.ai',
   headers: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
   methods: ['GET', 'HEAD', 'POST'],
   expose: [],
