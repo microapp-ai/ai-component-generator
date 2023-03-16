82509ff1ea7cc2a3cfa73caf2bc377ff7460750f
Carlos Knopel <carlos.knopel@gmail.com>
2023-03-16 09:14:08 -0300

    fix: add regular expression for CORS origin

--- cors.js
+++ cors.js
@@ -2,7 +2,9 @@ import cors from 'vercel-cors';
 
 export default cors({
   origin:
-    process.env.NODE_ENV === 'development' ? '*' : 'https://www.microapp.ai',
+    process.env.NODE_ENV === 'development'
+      ? '*'
+      : /^(https:\/\/)(www\.)?(([\w-]+)-microapp\.vercel\.app|microapp\.ai)$/,
   headers: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
   methods: ['GET', 'HEAD', 'POST'],
   expose: [],
