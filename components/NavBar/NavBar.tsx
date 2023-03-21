6dcb10e8970b4593b2521b188c24cb0dc2b9a2da
Carlos Knopel <carlos.knopel@gmail.com>
2023-03-21 01:07:05 -0300

    fix: add items to nav bar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,12 +1,21 @@
 import { FC } from 'react';
-import { Header, Group, Container } from '@mantine/core';
+import {
+  Header,
+  Group,
+  Container,
+  useMantineColorScheme,
+  Text,
+} from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import { ColorSchemeToggle, Logo } from '@/components';
 import { useStyles } from './styles';
+import Link from 'next/link';
 
 const NavBar: FC = () => {
   const { classes } = useStyles();
   const matches = useMediaQuery('(min-width: 992px)');
+  const { colorScheme } = useMantineColorScheme();
+  const isDark = colorScheme === 'dark';
 
   return (
     <>
@@ -19,6 +28,21 @@ const NavBar: FC = () => {
           </Group>
 
           <Group>
+            <Link href="/about" style={{ textDecoration: 'none' }}>
+              <Text size={16} weight={600} color={isDark ? '#fff' : '#202123'}>
+                About
+              </Text>
+            </Link>
+            <Text
+              color={isDark ? '#fff' : '#202123'}
+              size={16}
+              weight={600}
+              sx={{ textDecoration: 'none' }}
+              component="a"
+              href="mailto:invest@microapp.ai?subject=Hi - Microapp.ai"
+            >
+              Contact Us
+            </Text>
             <ColorSchemeToggle />
           </Group>
         </Container>
